e07bc537c680cf731d6f5aae1b27a676e38e5a53
Arthur Pointeau <arthur.pointeau@gmail.com>
2021-10-15 14:13:51 +0200

    [SAVE SCENE] Made the parent introduction GO a prefab. Add to scene. Hook Next button events

---
+++ Assets/IntroductionCanvases.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroductionCanvases : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _introductionCanvases;
+    
+    
+    public void NextButton()
+    {
+        
+    }
+}
