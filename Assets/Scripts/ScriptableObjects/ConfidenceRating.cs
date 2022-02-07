93929b81e0bfad23c831b20bd44781b1bf6cbe06
Arthur Pointeau <arthur.pointeau@gmail.com>
2022-02-07 16:36:37 +0100

    adapting prefabs

--- Assets/Scripts/ScriptableObjects/ConfidenceRating.cs
+++ Assets/Scripts/ScriptableObjects/ConfidenceRating.cs
@@ -13,6 +13,8 @@ public class ConfidenceRating : MonoBehaviour
     [SerializeField] private Slider _confidenceSlider;
     [SerializeField] private Response _response;
     [SerializeField] private ExperimentStage _experimentStage;
+
+    [SerializeField] private GameObject _answerGameObject;
     
     private Stopwatch _stopwatch;
     private void OnEnable()
@@ -41,6 +43,7 @@ public class ConfidenceRating : MonoBehaviour
 
     public void Show()
     {
+        _answerGameObject.SetActive(_experimentStage.stage == Stage.online);
         _stopwatch.Start();
         GetComponent<PanelDimmer>().Show();
         _confidenceSlider.value = 0.5f;    
